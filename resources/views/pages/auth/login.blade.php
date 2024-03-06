add25a541f61186a68b8424383d65379e210b4d1
Candra <candradigitalindo@gmail.com>
2024-03-07 06:58:21 +0700

    isntal & configurasi fortify

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Candio Management</title>
+    <link href="{{ asset('favicon.png') }}" rel="shortcut icon">
+    <title>Login Sistem Management Pelanggan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('auth/style.css') }}">
@@ -18,10 +19,11 @@
             <!--------------------------- Left Box ----------------------------->
             <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
                 style="background: #fad57a;">
-                <div class="featured-image mb-3">
-                    <img src="{{ asset('logo.png') }}" class="img-fluid" style="width: 250px;">
+                <div class="featured-image mt-2">
+                    <img src="{{ asset('logo.png') }}" class="img-fluid" style="width: 450px;">
                 </div>
-                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Manajamen Palanggan</p>
+                <p class="text-white fs-2 mb-2"
+                    style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Management Palanggan</p>
                 <small class="text-white text-wrap text-center"
                     style="width: 17rem;font-family: 'Courier New', Courier, monospace;"></small>
             </div>
@@ -33,28 +35,29 @@
                         <h2>Selamat Datang</h2>
                         <p>Waktunya kembali bekerja!</p>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control form-control-lg bg-light fs-6"
-                            placeholder="Email address">
-                    </div>
-                    <div class="input-group mb-1">
-                        <input type="password" class="form-control form-control-lg bg-light fs-6"
-                            placeholder="Password">
-                    </div>
-                    <div class="input-group mb-5 d-flex justify-content-between">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="formCheck">
-                            <label for="formCheck" class="form-check-label text-secondary"><small>Remember
-                                    Me</small></label>
+                    <form action="{{ route('login') }}" method="POST">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control form-control-lg bg-light fs-6"
+                                placeholder="Email address">
                         </div>
-                        <div class="forgot">
-                            <small><a href="#">Forgot Password?</a></small>
+                        <div class="input-group mb-1">
+                            <input type="password" class="form-control form-control-lg bg-light fs-6"
+                                placeholder="Password">
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
-                    </div>
-
+                        <div class="input-group mb-5 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="formCheck">
+                                <label for="formCheck" class="form-check-label text-secondary"><small>Remember
+                                        Me</small></label>
+                            </div>
+                            <div class="forgot">
+                                <small><a href="#">Forgot Password?</a></small>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button class="btn btn-lg primary w-100 fs-6 text-white">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
-Route::get('/', [AuthController::class, 'login'])->name('login');
+// Route::get('/', [AuthController::class, 'login'])->name('login');
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
